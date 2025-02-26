1bbea25dfb289079f66fbad9af312997d0543f47
TGSpock123 <tgspock123@outlook.com>
2025-02-26 09:01:50 +0800

    daily upload 2.26

---
+++ template_stm32h7_cubeide/Core/Src/freertos.c
@@ -0,0 +1,151 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+#include "SEGGER_RTT.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+/* Definitions for task_default */
+osThreadId_t task_defaultHandle;
+const osThreadAttr_t task_default_attributes = {
+  .name = "task_default",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for queue_default */
+osMessageQueueId_t queue_defaultHandle;
+const osMessageQueueAttr_t queue_default_attributes = {
+  .name = "queue_default"
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void task_default_start(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the queue(s) */
+  /* creation of queue_default */
+  queue_defaultHandle = osMessageQueueNew (1, sizeof(uint32_t), &queue_default_attributes);
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of task_default */
+  task_defaultHandle = osThreadNew(task_default_start, NULL, &task_default_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_task_default_start */
+/**
+  * @brief  Function implementing the task_default thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_task_default_start */
+void task_default_start(void *argument)
+{
+  /* USER CODE BEGIN task_default_start */
+  SEGGER_RTT_printf(0, "Hello STM32H743. \r\n");
+  /* Infinite loop */
+  for(;;)
+  {
+    log_a("Hello Elog. \n");
+    vTaskDelay(200);
+    log_d("Hello Elog. \n");
+    vTaskDelay(200);
+    log_e("Hello Elog. \n");
+    vTaskDelay(200);
+    log_i("Hello Elog. \n");
+    vTaskDelay(200);
+    log_v("Hello Elog. \n");
+    vTaskDelay(200);
+    log_w("Hello Elog. \n");
+    vTaskDelay(200);
+  }
+  /* USER CODE END task_default_start */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
